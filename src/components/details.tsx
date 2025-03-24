bf0726c4ec82a5955fb43114f06059aff551fb61
Aswin S <aswinsanthoshachus@gmail.com>
2025-03-24 08:40:06 +0530

    Update details.tsx

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -11,9 +11,9 @@ type List = {
 export const list: List[] = [
     {
       id: 1,
-      head:"latest in tech news",
-      sub:"2019 Podcast Awards Technology Finalist! Tune in for the latest trending news in tech and gadgets. We search the web for only the newest and coolest news to share with you, even if you are on the go. Welcome to the community and feel free to stay in touch with the show.Electronics & IT Minister Ravi Shankar Prasad believes India’s digital ecosystem will contribute $1 trillion to the $5 trillion economy targeted in five years.",
-      datas:"hello i am a something new thing this is one ",
+      head:"21,413 ഒഴിവുകള്‍: ഗ്രാമീണ്‍ ഡാക് സേവക് ഫലം പ്രസിദ്ധീകരിച്ചു",
+      sub:"from - mathrubhumi.com",
+      datas:"ന്യൂഡൽഹി: ഗ്രാമീൺ ഡാക് സേവക് (ജിഡിഎസ്) 2025 ഫലം പ്രസിദ്ധീകരിച്ചു. ഗ്രാമീൺ ഡാക് സേവക് (ജിഡിഎസ്) ജനുവരി 2025 മെറിറ്റ് ലിസ്റ്റ് ആണ് ഇന്ത്യാ പോസ്റ്റ് പുറത്തുവിട്ടിരിക്കുന്നത്. 22 സംസ്ഥാനങ്ങളിലേക്ക് ഷോർട്ട്ലിസ്റ്റ് ചെയ്യപ്പെട്ട ഉദ്യോഗാർഥികളുടെ വിവരങ്ങളാണ് പ്രസിദ്ധീകരിച്ചിരിക്കുന്നത്. ഗ്രാമീൺ ഡാക് സേവക് ഒഴിവുകളിലേക്ക് അപേക്ഷിച്ചവർക്ക് ഇന്ത്യ പോസ്റ്റിന്റെ ഔദ്യോഗിക വെബ്സൈറ്റിലൂടെ മെറിറ്റ് ലിസ്റ്റ് പരിശോധിക്കാം. <br> ആന്ധ്രപ്രദേശ്, അസം, ബിഹാർ, ചത്തീസ്ഗഢ്, ഡൽഹി, ഗുജറാത്ത്, ഹരിയാണ, ഹിമാചൽപ്രദേശ്, കർണാടക, കേരള, മധ്യപ്രദേശ്, തമിഴ്നാട്, തെലങ്കാന, ഉത്തർപ്രദേശ്, ഉത്തരാഖണ്ഡ് തുടങ്ങിയ സംസ്ഥാനങ്ങളിലേക്ക് ഗ്രാമീൺ ഡാക് സേവക് ഒഴിവുകളിലേക്ക് ഷോർട്ടലിസ്റ്റ് ചെയ്തിരിക്കുന്ന ഉദ്യോഗാർഥികളുടെ വിവരങ്ങളാണ് പ്രസിദ്ധീകരിച്ചിരിക്കുന്നത് ",
       image: image1,
     },
     {
