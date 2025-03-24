7a19c449e724f28e18122d2ac97268918cdb97ff
Aswin S <aswinsanthoshachus@gmail.com>
2025-03-24 13:32:52 +0530

    Update sections.tsx

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -2,14 +2,16 @@ import Image, { StaticImageData } from "next/image"
 import image from "../assests/news.jpg"
 import { list } from "@/components/details";
 
-export const Section = ({head,id,datas}:{
-  head:string,datas:string,id:number,
+export const Section = ({head,id,image,datas}:{
+  head:string,datas:string,id:number,image:StaticImageData,
 }) => {
     return(    
 <div  className="notification mt-8 mb-8 ">
     <div className="notiglow"></div>
     <div className="notiborderglow"></div>
-    <div className="notiimage"> </div>
+    <div className="notiimage"> 
+    <Image src={image} alt='news' className=" notipic  image-media object-fit" />
+</div>
     <div className="notititle">{head}</div>
     <div className="notibody">{datas}</div>
   </div>
